5bff886d0a4537826457b6469ccf5fcc5f10f6ba
Yan-1999 <49827960+Yan-1999@users.noreply.github.com>
2021-06-08 00:14:57 +0800

    task stage, moving task, del single task, progress

--- script.js
+++ script.js
@@ -2,69 +2,197 @@ var Stage;
 (function (Stage) {
     Stage[Stage["Backlog"] = 0] = "Backlog";
     Stage[Stage["Todo"] = 1] = "Todo";
-    Stage[Stage["InProgress"] = 2] = "InProgress";
+    Stage[Stage["Doing"] = 2] = "Doing";
     Stage[Stage["Verify"] = 3] = "Verify";
     Stage[Stage["Done"] = 4] = "Done";
 })(Stage || (Stage = {}));
 var STROAGE_KEY = 'tenparu';
 var task_template = document.getElementById('task-template');
 var task_list = document.getElementById('task-list');
+var curStage = Stage.Doing;
+var tasks = {};
+function taskAncestorElement(elem) {
+    var e = elem;
+    while (e !== document.body) {
+        if (e.classList.contains('task')) {
+            return (e);
+        }
+        e = e.parentElement;
+    }
+    return null;
+}
+function diffTimeStr(datetime) {
+    var diff = Date.now() - datetime.getTime();
+    var suffix = 'passed';
+    if (diff < 0) {
+        diff = -diff;
+        suffix = 'left';
+    }
+    if (diff > 86400000) {
+        return [Math.floor(diff / 86400000).toString(),
+            'days',
+            suffix].join(' ');
+    }
+    else if (diff > 3600000) {
+        return [Math.floor(diff / 3600000).toString(),
+            'hours',
+            suffix].join(' ');
+    }
+    else {
+        return [Math.floor(diff / 60000).toString(),
+            'minutes',
+            suffix].join(' ');
+    }
+}
+function writeBack() {
+    window.localStorage.setItem(STROAGE_KEY, JSON.stringify(tasks));
+}
 var Task = /** @class */ (function () {
     function Task(name, due, priority, description, stage) {
+        this.progress = 0;
         this.name = name;
         this.due = due;
         this.priority = priority;
         this.description = description;
         this.stage = stage;
     }
-    Task.prototype.render_div = function (div) {
+    Task.prototype.renderDiv = function (div) {
+        div.setAttribute('data-name', this.name);
         div.getElementsByClassName('task-item-name')[0].textContent = this.name;
-        div.getElementsByClassName('task-item-due')[0].textContent = this.due.toISOString();
+        div.getElementsByClassName('task-item-due')[0].textContent = this.due.toLocaleString();
         div.getElementsByClassName('task-item-priority')[0].textContent = this.priority;
         div.getElementsByClassName('task-item-description')[0].textContent = this.description;
+        div.getElementsByClassName('task-item-timeleft')[0].textContent = diffTimeStr(this.due);
+        div.getElementsByClassName('task-item-delete')[0].onclick = this.remove.bind(this);
+        var next_button = div.getElementsByClassName('task-item-next')[0];
+        var range = div.getElementsByClassName('task-item-progress')[0];
+        var stage = this.stage;
+        if (stage === Stage.Done) {
+            next_button.style.display = 'none';
+            range.style.display = 'none';
+            div.setAttribute('data-done', '');
+        }
+        else {
+            next_button.textContent = Stage[stage + 1];
+            next_button.onclick = this.advanceStage.bind(this);
+            range.onchange = this.setProgress.bind(this);
+            range.value = this.progress.toString();
+            if (range.value === range.max) {
+                div.setAttribute('data-done', '');
+            }
+        }
     };
-    Task.prototype.new_div = function () {
+    Task.prototype.newDiv = function () {
         var node = task_template.content.querySelector('.task');
-        this.render_div(node);
-        return document.importNode(node, true);
+        var new_node = document.importNode(node, true);
+        this.renderDiv(new_node);
+        return new_node;
+    };
+    Task.prototype.remove = function (ev) {
+        var div = taskAncestorElement(ev.target);
+        div.remove();
+        delete tasks[this.name];
+        writeBack();
+    };
+    Task.prototype.setProgress = function (ev) {
+        var range = ev.target;
+        var val = range.value;
+        this.progress = Number.parseInt(val);
+        var task_div = taskAncestorElement(range);
+        if (range.value === range.max) {
+            task_div.setAttribute('data-done', '');
+        }
+        else {
+            task_div.removeAttribute('data-done');
+        }
+        writeBack();
+    };
+    Task.prototype.setStage = function (ev, stage) {
+        var div = taskAncestorElement(ev.target);
+        div.remove();
+        this.stage = stage;
+        writeBack();
+    };
+    Task.prototype.advanceStage = function (ev) {
+        this.setStage(ev, this.stage + 1);
     };
     return Task;
 }());
 function addTask(ev) {
     var form = ev.target;
     var data = new FormData(form);
-    var tasks = JSON.parse(window.localStorage.getItem(STROAGE_KEY));
-    if (tasks === null) {
-        tasks = [];
-    }
     var name = data.get('task-name').toString();
-    for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
-        var task = tasks_1[_i];
-        if (task.name === name) {
-            alert("Task with the name already exits!");
-            return false;
-        }
+    if (name in tasks) {
+        alert("Task with the name already exits!");
+        return false;
     }
     var new_task = new Task(name, new Date([
         data.get('due-date').toString(),
         data.get('due-time').toString(),
-    ].join(' ')), data.get('priority').toString(), data.get('description').toString(), Stage.Todo);
-    tasks.push(new_task);
-    window.localStorage.setItem(STROAGE_KEY, JSON.stringify(tasks));
-    var div = new_task.new_div();
+    ].join(' ')), data.get('priority').toString(), data.get('description').toString(), Stage.Doing);
+    tasks[name] = new_task;
+    writeBack();
+    var div = new_task.newDiv();
     task_list.appendChild(div);
     return false;
 }
-function loadTask() {
-    var tasks = JSON.parse(window.localStorage.getItem(STROAGE_KEY));
-    var div = null;
-    for (var _i = 0, tasks_2 = tasks; _i < tasks_2.length; _i++) {
-        var task = tasks_2[_i];
-        task.due = new Date(task.due);
-        div = Object.assign(new Task(), task).new_div();
-        task_list.appendChild(div);
+function loadTaskList(stage) {
+    task_list.innerHTML = '';
+    for (var key in tasks) {
+        var task = tasks[key];
+        if (task.stage === stage) {
+            task_list.appendChild(task.newDiv());
+        }
+    }
+}
+function load() {
+    if (tasks_str !== null) {
+        var raw_list = JSON.parse(window.localStorage.getItem(STROAGE_KEY));
+        for (var key in raw_list) {
+            var raw = raw_list[key];
+            raw.due = new Date(raw.due);
+            var task = tasks[key] = new Task();
+            Object.assign(task, raw);
+        }
+    }
+}
+function changeStage(stage) {
+    loadTaskList(stage);
+    for (var index = 0; index <= Stage.Done; index++) {
+        var elem = document.getElementById(['stage-',
+            Stage[index].toLowerCase()].join(''));
+        if (index === stage) {
+            elem.setAttribute('data-current', '');
+        }
+        else {
+            elem.removeAttribute('data-current');
+        }
+    }
+    curStage = stage;
+}
+function doneAllCurStage() {
+    for (var key in tasks) {
+        var task = tasks[key];
+        if (task.stage === curStage) {
+            task.stage = Stage.Done;
+        }
     }
 }
 /*main*/
 document.getElementById('new-task').onsubmit = addTask;
-loadTask();
+var stages_div = document.getElementById('stages');
+for (var index = 0; index <= Stage.Done; index++) {
+    var stage_str = Stage[index];
+    var anchor = document.createElement('a');
+    anchor.setAttribute('herf', '#');
+    anchor.id = ['stage-', stage_str.toLowerCase()].join('');
+    anchor.textContent = stage_str;
+    anchor.onclick = changeStage.bind(null, index);
+    stages_div.appendChild(anchor);
+}
+var tasks_str = window.localStorage.getItem(STROAGE_KEY);
+if (tasks_str !== null) {
+    tasks = JSON.parse(window.localStorage.getItem(STROAGE_KEY));
+}
+load();
+changeStage(curStage);
--- script.ts
+++ script.ts
@@ -1,9 +1,52 @@
-enum Stage { Backlog, Todo, InProgress, Verify, Done }
+enum Stage { Backlog, Todo, Doing, Verify, Done }
 
 const STROAGE_KEY = 'tenparu';
 
-var task_template = <HTMLTemplateElement>document.getElementById('task-template');
-var task_list = <HTMLDivElement>document.getElementById('task-list');
+var task_template = document.getElementById('task-template') as
+    HTMLTemplateElement;
+var task_list = document.getElementById('task-list') as HTMLDivElement;
+var curStage = Stage.Doing;
+var tasks: { [key: string]: Task } = {};
+
+function taskAncestorElement(elem: Element) {
+    let e = elem;
+    while (e !== document.body) {
+        if (e.classList.contains('task')) {
+            return (e);
+        }
+        e = e.parentElement;
+    }
+    return null;
+}
+
+function diffTimeStr(datetime: Date) {
+    let diff = Date.now() - datetime.getTime();
+    let suffix = 'passed';
+
+    if (diff < 0) {
+        diff = -diff;
+        suffix = 'left';
+    }
+    if (diff > 86400000) {
+        return [Math.floor(diff / 86400000).toString(),
+            'days',
+            suffix].join(' ');
+    }
+    else if (diff > 3600000) {
+        return [Math.floor(diff / 3600000).toString(),
+            'hours',
+            suffix].join(' ');
+    }
+    else {
+        return [Math.floor(diff / 60000).toString(),
+            'minutes',
+            suffix].join(' ');
+    }
+}
+
+function writeBack() {
+    window.localStorage.setItem(STROAGE_KEY, JSON.stringify(tasks));
+}
 
 class Task {
     name: string;
@@ -11,6 +54,7 @@ class Task {
     priority: string;
     description: string;
     stage: Stage;
+    progress: number = 0;
 
     constructor(name?: string,
         due?: Date,
@@ -24,37 +68,90 @@ class Task {
         this.stage = stage;
     }
 
-    render_div(div: HTMLDivElement) {
+    renderDiv(div: HTMLDivElement) {
+        div.setAttribute('data-name', this.name);
         div.getElementsByClassName(
             'task-item-name')[0].textContent = this.name;
         div.getElementsByClassName(
-            'task-item-due')[0].textContent = this.due.toISOString();
+            'task-item-due')[0].textContent = this.due.toLocaleString();
         div.getElementsByClassName(
             'task-item-priority')[0].textContent = this.priority;
         div.getElementsByClassName(
             'task-item-description')[0].textContent = this.description;
+        div.getElementsByClassName(
+            'task-item-timeleft')[0].textContent = diffTimeStr(this.due);
+
+        (div.getElementsByClassName('task-item-delete')[0] as
+            HTMLButtonElement).onclick = this.remove.bind(this);
+
+        let next_button = div.getElementsByClassName('task-item-next')[0] as
+            HTMLButtonElement;
+        let range = div.getElementsByClassName('task-item-progress')[0] as
+            HTMLInputElement
+        let stage = this.stage;
+        if (stage === Stage.Done) {
+            next_button.style.display = 'none';
+            range.style.display = 'none';
+            div.setAttribute('data-done', '');
+        } else {
+            next_button.textContent = Stage[stage + 1];
+            next_button.onclick = this.advanceStage.bind(this);
+            range.onchange = this.setProgress.bind(this);
+            range.value = this.progress.toString();
+            if (range.value === range.max) {
+                div.setAttribute('data-done', '');
+            }
+        }
     }
 
-    new_div() {
-        let node = task_template.content.querySelector<HTMLDivElement>('.task');
-        this.render_div(node);
-        return document.importNode(node, true);
+    newDiv() {
+        let node = task_template.content.querySelector<HTMLDivElement>(
+            '.task');
+        let new_node = document.importNode(node, true);
+        this.renderDiv(new_node);
+        return new_node;
+    }
+
+    remove(ev: Event) {
+        let div = taskAncestorElement(ev.target as HTMLInputElement);
+        div.remove();
+        delete tasks[this.name];
+        writeBack();
+    }
+
+    setProgress(ev: Event) {
+        let range = ev.target as HTMLInputElement;
+        let val = range.value;
+        this.progress = Number.parseInt(val);
+        let task_div = taskAncestorElement(range);
+        if (range.value === range.max) {
+            task_div.setAttribute('data-done', '');
+        } else {
+            task_div.removeAttribute('data-done');
+        }
+        writeBack();
+    }
+
+    setStage(ev: Event, stage: Stage) {
+        let div = taskAncestorElement(ev.target as HTMLInputElement);
+        div.remove();
+        this.stage = stage;
+        writeBack();
+    }
+
+    advanceStage(ev: Event) {
+        this.setStage(ev, this.stage + 1);
     }
 }
 
+
 function addTask(ev: Event) {
-    let form: HTMLFormElement = <HTMLFormElement>ev.target;
+    let form: HTMLFormElement = ev.target as HTMLFormElement;
     let data = new FormData(form);
-    let tasks: Task[] = JSON.parse(window.localStorage.getItem(STROAGE_KEY));
-    if (tasks === null) {
-        tasks = [];
-    }
     let name = data.get('task-name').toString();
-    for (const task of tasks) {
-        if (task.name === name) {
-            alert("Task with the name already exits!")
-            return false;
-        }
+    if (name in tasks) {
+        alert("Task with the name already exits!")
+        return false;
     }
     let new_task = new Task(
         name,
@@ -64,25 +161,76 @@ function addTask(ev: Event) {
         ].join(' ')),
         data.get('priority').toString(),
         data.get('description').toString(),
-        Stage.Todo
+        Stage.Doing
     );
-    tasks.push(new_task);
-    window.localStorage.setItem(STROAGE_KEY, JSON.stringify(tasks));
-    let div = new_task.new_div();
+    tasks[name] = new_task;
+    writeBack();
+    let div = new_task.newDiv();
     task_list.appendChild(div);
     return false;
 }
 
-function loadTask() {
-    let tasks = JSON.parse(window.localStorage.getItem(STROAGE_KEY));
-    let div: HTMLDivElement = null;
-    for (const task of tasks) {
-        task.due = new Date(task.due);
-        div = Object.assign(new Task(), task).new_div();
-        task_list.appendChild(div);
+function loadTaskList(stage: Stage) {
+    task_list.innerHTML = '';
+    for (const key in tasks) {
+        const task = tasks[key];
+        if (task.stage === stage) {
+            task_list.appendChild(task.newDiv());
+        }
     }
+}
+
+function load() {
+    if (tasks_str !== null) {
+        let raw_list = JSON.parse(window.localStorage.getItem(STROAGE_KEY));
+        for (const key in raw_list) {
+            const raw = raw_list[key];
+            raw.due = new Date(raw.due);
+            let task = tasks[key] = new Task();
+            Object.assign(task, raw);
+        }
+    }
+}
+
+function changeStage(stage: Stage) {
+    loadTaskList(stage);
+    for (let index = 0; index <= Stage.Done; index++) {
+        let elem = document.getElementById(['stage-',
+            Stage[index].toLowerCase()].join(''));
+        if (index === stage) {
+            elem.setAttribute('data-current', '');
+        } else {
+            elem.removeAttribute('data-current');
+        }
+    }
+    curStage = stage;
+}
+
+function doneAllCurStage() {
+    for (const key in tasks) {
+        const task = tasks[key];
+        if (task.stage === curStage) {
+            task.stage = Stage.Done;
+        }
+    }
+
 }
 
 /*main*/
 document.getElementById('new-task').onsubmit = addTask;
-loadTask();
+let stages_div = document.getElementById('stages');
+for (let index = 0; index <= Stage.Done; index++) {
+    const stage_str = Stage[index];
+    let anchor = document.createElement('a');
+    anchor.setAttribute('herf', '#');
+    anchor.id = ['stage-', stage_str.toLowerCase()].join('');
+    anchor.textContent = stage_str;
+    anchor.onclick = changeStage.bind(null, index);
+    stages_div.appendChild(anchor);
+}
+let tasks_str = window.localStorage.getItem(STROAGE_KEY);
+if (tasks_str !== null) {
+    tasks = JSON.parse(window.localStorage.getItem(STROAGE_KEY))
+}
+load();
+changeStage(curStage);
